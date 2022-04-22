3affbf0e6c41859a910e2fab9af7262fdb2276d9
SIDDHANT RUSTAGI <siddhant.rustagi@gmail.com>
2022-04-22 21:17:13 +0530

    Initial Commit for backend

---
+++ backend/TA/TA/src/main/java/com/agents/TA/TaApplication.java
@@ -0,0 +1,13 @@
+package com.agents.TA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaApplication.class, args);
+	}
+
+}
